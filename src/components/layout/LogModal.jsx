f19106e2d0408b76d554af0f7573a016ee1ad2f6
Tare885 <118376741+Tare885@users.noreply.github.com>
2022-12-07 19:44:08 +0100

    Modify Login Modal

--- src/components/layout/LogModal.jsx
+++ src/components/layout/LogModal.jsx
@@ -8,7 +8,7 @@ function LogModal({ onClose2, visible2 }) {
     <div className="fixed inset-0 bg-zinc-900/80 backdrop-blur-sm flex justify-center items-center z-10">
       <MdClose
         onClick={onClose2}
-        className="absolute top-0 right-0 m-10 text-2xl sm:text-4xl md:text-4xl lg:text-6xl cursor-pointer text-orange-500"
+        className="absolute top-0 right-0 hover:scale-110 transition-all m-10 text-2xl sm:text-4xl md:text-4xl lg:text-6xl cursor-pointer text-orange-500"
       />
       <div className="border border-gray-500 rounded-lg p-10">
         <h1 className="text-center text-3xl mb-5">SIGN IN</h1>
@@ -50,19 +50,19 @@ function LogModal({ onClose2, visible2 }) {
                 Last name
               </label>
             </div>
-            <div class="flex items-start mb-5">
-              <div class="flex items-center h-5">
+            <div className="flex items-start mb-5">
+              <div className="flex items-center h-5">
                 <input
                   id="remember"
                   type="checkbox"
                   value=""
-                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
                   required
                 />
               </div>
               <label
                 for="remember"
-                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
                 Remember me
               </label>
